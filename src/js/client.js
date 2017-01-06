bfe12bec35c58291e4cab6da374b4f12b1b9a8ad
Ernest Iliiasov <me@ernest.asia>
2017-01-06 03:21:51 -0500

    configure webpack sass and organize files

--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import style from "../css/style.sass";
 
 import App from "./components/App";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, "src"),
@@ -16,16 +17,28 @@ module.exports = {
           presets: ['react', 'es2015', 'stage-0'],
           plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
         }
-      }
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+      },
     ]
   },
   output: {
-    path: __dirname + "/src/",
+    path: __dirname + "/src/built/",
     filename: "client.min.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+  plugins: [
+    new ExtractTextPlugin('style.css', {
+      allChunks: true
+    }),
   ],
 };
